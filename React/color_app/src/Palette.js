2213b543159d165f4ca0f4a15f1f16926cd8b29a
ankit.podder <ankitpodder0211@gmail.com>
2023-02-19 21:43:42 +0530

    add Palette component in color-app

---
+++ React/color_app/src/Palette.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Palette extends Component {
+  render() {
+    return (
+      <div className='palette-container'>
+        <div className='color-box'>
+          <p>Color Box</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Palette;
