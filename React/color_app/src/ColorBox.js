c00d660a935e7355f794c27d2be33b70bc36ecca
ankit.podder <ankitpodder0211@gmail.com>
2023-02-19 23:50:13 +0530

    add copy functionality

--- React/color_app/src/ColorBox.js
+++ React/color_app/src/ColorBox.js
@@ -1,18 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import CopyToClipboard from "react-copy-to-clipboard";
 
-import './css/ColorBox.css'
+import "./css/ColorBox.css";
 
 class ColorBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      copied: false,
+    };
+    this.handleCopy = this.handleCopy.bind(this);
+  }
+  handleCopy() {
+    this.setState({ copied: true }, () => {
+      setTimeout(() => this.setState({ copied: false }), 1500);
+    });
+  }
   render() {
+    let { name, color } = this.props;
     return (
-      <div className='color-box' style={{backgroundColor: this.props.color}}>
-        <div className='copy-container'>
-          <p className='copy'>Copy</p>
-          <p className='color-name'>{this.props.name}</p>
-          <p className='more'>More</p>
+      <CopyToClipboard text={color} onCopy={this.handleCopy}>
+        <div className="color-box" style={{ backgroundColor: color }}>
+          <div className="copy-container">
+            <p className="copy">Copy</p>
+            <p className="color-name">{name}</p>
+            <p className="more">More</p>
+          </div>
+          <div
+            className={`overlay ${this.state.copied && "active"}`}
+            style={{ backgroundColor: color }}
+          ></div>
+          <div className={`copied-content ${this.state.copied && "active"}`}>
+            <p className="copied">Copied</p>
+            <p className="colorname">{color}</p>
+          </div>
         </div>
-      </div>
-    )
+      </CopyToClipboard>
+    );
   }
 }
 
