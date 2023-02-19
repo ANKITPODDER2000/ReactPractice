c911124179e56ffd32ac899846e0e77df325d3ce
ankit.podder <ankitpodder0211@gmail.com>
2023-02-20 00:35:37 +0530

    create script to create diff shades of color

--- React/color_app/src/App.js
+++ React/color_app/src/App.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react';
-import Palette from './Palette';
-import SeedColors from './SeedColors';
+import React, { Component } from "react";
+import Palette from "./Palette";
+import SeedColors from "./SeedColors";
+import getUpdatedPalette from "./GetUpdatedPalette";
 
-import './css/App.css'
+import "./css/App.css";
 
 class App extends Component {
   render() {
+    let updatedColor = getUpdatedPalette(SeedColors[4]);
     return (
-      <div className='app-container'>
+      <div className="app-container">
         <Palette {...SeedColors[4]} />
       </div>
-    )
+    );
   }
 }
-
 
 export default App;
---
+++ React/color_app/src/GetUpdatedPalette.js
@@ -0,0 +1,39 @@
+import chroma, { hex } from "chroma-js";
+
+let getAllColorShade = (hexColor, noOffColors) => {
+  return chroma
+    .scale([chroma(hexColor).brighten(2), chroma(hexColor).darken(2)])
+    .mode("lch")
+    .colors(noOffColors);
+};
+let getUpdatedPalette = (palette) => {
+  let colorPalette = {
+    paletteName: palette.paletteName,
+    id: palette.id,
+    emoji: palette.emoji,
+    colorShade: {},
+  };
+
+  let colorRange = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
+  for (let colorLevel of colorRange) {
+    colorPalette.colorShade[colorLevel] = [];
+  }
+
+  console.log(colorPalette);
+
+  for (let color of palette.colors) {
+    let hexColor = color.color;
+    let allShades = getAllColorShade(hexColor, colorRange.length);
+    for (let i = 0; i < colorRange.length; i++) {
+      colorPalette.colorShade[colorRange[i]].push({
+        name: `${color.name} ${colorRange[i]}`,
+        color: allShades[i],
+        rgbColor: chroma(allShades[i]).css(),
+      });
+    }
+  }
+
+  return colorPalette;
+};
+
+export default getUpdatedPalette;
